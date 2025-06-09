4651ae4d3989f2e2f792386d707d50f4ff4220e0
Johab Freitas <freitasjohab@gmail.com>
2025-06-09 15:44:32 -0300

    Corrigindo falha

--- src/Main.java
+++ src/Main.java
@@ -22,33 +22,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /*Gson gson = new Gson();
-        String json = gson.toJson(new String[] {"Java", "Gson", "Modules"});
-        System.out.println(json);*/
-
         Properties props = new Properties();
         props.load(new FileInputStream("config.properties"));
         String apiKey = props.getProperty("api.key");
 
         // Setting URL
-        final String API_KEY = "180c181f3fddda1bcbfac0db";
         String url_str = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
-
-        /*
-        // Making Request
-        URL url = new URL(url_str);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
-
-        // Convert to JSON
-        JsonParser jp = new JsonParser();
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-        JsonObject jsonobj = root.getAsJsonObject();
-
-        // Accessing object
-        String req_result = jsonobj.get("documentation").getAsString();
-        System.out.println(req_result);
-        */
 
         //Uma nova requisição
 
